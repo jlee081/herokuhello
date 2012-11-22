7d0ce909c6a58deedcc1a83a7088125a7c8ecf47
Jason Lee <jjasonglee@gmail.com>
2012-11-21 20:03:23 -0800

    this is the first version of hello world

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+	return "welcome home"	
+
+@app.route("/jason")
+def jason():
+    return "wassssuuuppp Jason"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", "80"))
+    app.run(port=port)
